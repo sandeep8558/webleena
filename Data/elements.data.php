827990d6cf5b744dd0cbb0fdf96687134882d734
sandeep8558 <sandeep198558@gmail.com>
2023-12-29 23:01:32 +0530

    Layout Connected

---
+++ Data/elements.data.php
@@ -0,0 +1,25 @@
+<?php
+$element = [
+    "name" => "elements",
+    "type" => "recursive",
+    "fields" => [
+        [
+            "name" => "name",
+            "text" => "Element Name",
+            "type" => "text",
+            "value" => "",
+            "validation" => "",
+            "grid" => "Yes",
+            "form" => "Yes"
+        ],
+        [
+            "name" => "type",
+            "text" => "Element Type",
+            "type" => "text",
+            "value" => "",
+            "validation" => "",
+            "grid" => "Yes",
+            "form" => "Yes"
+        ]
+    ]
+];
